cb41f6288dd63c7bcf6b29923e5526e7aa5a9832
digitechgrouperu <digitechgrouperu@gmail.com>
2026-02-19 19:22:05 -0500

    updates

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Laravel Subdirectory Bridge
+ * Este archivo permite que el proyecto funcione en Hostinger 
+ * sin necesidad de redirecciones complejas en el .htaccess
+ */
+require __DIR__ . '/public/index.php';
